c47849e15e1ec4824f9c37d67cbc0de81b6f40a8
sachinmd0111 <59419638+sachinmd0111@users.noreply.github.com>
2020-07-16 01:48:42 +0530

    Update area of circle.py

--- area of circle.py
+++ area of circle.py
@@ -1,10 +1,19 @@
-
+#PROGRAM NUMBER 1
 import math
 fname=float(input("Input the radius of the circle "))
 area= math.pi*fname**2
 print("The area of the circle with radius",fname,"is ",area)
             
-            
+ #PROGRAM NUMBER 2
+fname=input("Input the filename:" )
+a=fname.split('.')
+b=a[1]
+if b=='py':
+ c=b+'thon'
+ print("The extension of the file is:",c)
+else:
+ print("SORRY PLEASE TRY AGAIN")          
+
             
             
         
